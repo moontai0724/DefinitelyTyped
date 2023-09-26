56ada50ab74ab9dd7bcaf0ebff1baf42f74b5d4d
moontai0724 <moontai0724@gmail.com>
2023-09-26 10:50:47 +0800

    test: openapi spec v3.0
    https://github.com/OAI/OpenAPI-Specification/tree/3.0.3/examples/v3.0

---
+++ types/openapis/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for openapis 3.0
+// Project: https://github.com/OAI/OpenAPI-Specification
+// Definitions by: moontai0724 <https://github.com/moontai0724>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export {};
